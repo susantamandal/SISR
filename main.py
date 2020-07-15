baae65f1aa8126633094a7ba16527a1fb4a484f7
Susanta <57414214+susantamandal@users.noreply.github.com>
2020-07-15 20:02:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ flags.DEFINE_integer('total_epoch', 0, 'Total training epoch')
 flags.DEFINE_integer('batch_size', 0, 'Batch_size')
 flags.DEFINE_integer('no_of_batches', 0, 'Number of batches')
 flags.DEFINE_integer('save_interval', 0, 'Save model interval')
-flags.DEFINE_integer('size', 128, 'LR size')
+flags.DEFINE_integer('size', 128, 'LR image size')
 flags.DEFINE_integer('scale', 4, 'scaling factor')
-flags.DEFINE_string('dir_train_in', 'Training_inputs/SR_target', 'Input folder for training')
-flags.DEFINE_string('dir_val_in', 'Validation_inputs/Test', 'Input folder for Validation')
-flags.DEFINE_string('dir_val_target', 'Validation_inputs/Target', 'Target folder for Validation')
+flags.DEFINE_string('dir_train_in', 'Training_inputs/RGB_Target', 'Input folder for training')
+flags.DEFINE_string('dir_val_in', 'Validation_inputs/RGB_Test', 'Input folder for Validation')
+flags.DEFINE_string('dir_val_target', 'Validation_inputs/RGB_Target', 'Target folder for Validation')
 CONFIG=flags.FLAGS
 
 def main(args):
