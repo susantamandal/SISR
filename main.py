f238e5847f1d9be21a1e2a26045bdb8dc0701020
Susanta <57414214+susantamandal@users.noreply.github.com>
2020-07-15 19:03:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ flags.DEFINE_integer('no_of_batches', 0, 'Number of batches')
 flags.DEFINE_integer('save_interval', 0, 'Save model interval')
 flags.DEFINE_integer('size', 128, 'LR size')
 flags.DEFINE_integer('scale', 4, 'scaling factor')
-flags.DEFINE_string('dir_train_in', 'Training_inputs', 'Input folder for training')
+flags.DEFINE_string('dir_train_in', 'Training_inputs/SR_target', 'Input folder for training')
 flags.DEFINE_string('dir_val_in', 'Validation_inputs/Test', 'Input folder for Validation')
 flags.DEFINE_string('dir_val_target', 'Validation_inputs/Target', 'Target folder for Validation')
 CONFIG=flags.FLAGS
